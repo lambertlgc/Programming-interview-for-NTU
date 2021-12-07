cfca16ee51ba3721d7bf95701e96bcb675010043
lambertlgc <95503790+lambertlgc@users.noreply.github.com>
2021-12-07 01:26:21 +0000

    Add files via upload

---
+++ Question2/equations.m
@@ -0,0 +1,10 @@
+function dy=equations(t,y,flag,k_1,k_2,k_3)
+e_0=1;
+s_0=10;
+es_0=0;
+p_0=0;
+dy=[k_2*es_0+k_3*es_0-k_1*s_0*e_0
+    -k_1*s_0*e_0+k_2*es_0
+    k_1*e_0*s_0-k_2*es_0-k_3*es_0
+    k_3*es_0];
+
---
+++ Question2/integrate.m
@@ -0,0 +1,5 @@
+yzero=[1,10,0,0];
+k_1=100;k_2=600;k_3=150;
+tspan=[0,1000];
+[t,y]=ode45('equations',tspan,yzero,[],k_1,k_2,k_3);
+n=length(t);
